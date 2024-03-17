c2b040c8aa99ac03c44d44cf4230c04880d97f61
shofikul <179398ssshofik@gmail.com>
2024-03-17 15:25:09 +0600

    seperate req url,query,headers and body data from req property

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
 
 // app object - module scaffolding
 const app = {};
@@ -15,7 +17,26 @@ app.createServer = () => {
 };
 
 app.handleRequestResponse = (req, res) => {
-  console.log("Hello World!");
+  //get request url
+  const parseUrl = url.parse(req.url, true);
+  const pathname = parseUrl.pathname;
+  const trimmedPath = pathname.replace(/^\/+|\/+$/g, "");
+  const queryStringObject = parseUrl.query;
+  const reqMethod = req.method.toLowerCase();
+  const headerObject = req.headers;
+
+  const reqBodyDecoder = new StringDecoder();
+
+  let reqBodyData = "";
+
+  req.on("data", (buffer) => {
+    reqBodyData += reqBodyDecoder.write(buffer);
+  });
+
+  req.on("end", () => {
+    reqBodyData += reqBodyDecoder.end();
+  });
+
   res.end("Hello World!");
 };
 

32bbe54fb5a9c9ed2aad2754ad044b02ecf30a40
shofikul <179398ssshofik@gmail.com>
2024-03-17 14:57:35 +0600

    create http server with raw node js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const http = require("http");
-const server = http.createServer((req, res) => {
+
+// app object - module scaffolding
+const app = {};
+
+app.config = {
+  port: 3000,
+};
+
+app.createServer = () => {
+  const server = http.createServer(app.handleRequestResponse);
+  server.listen(app.config.port, () => {
+    console.log(`Server is running on port ${app.config.port}`);
+  });
+};
+
+app.handleRequestResponse = (req, res) => {
   console.log("Hello World!");
   res.end("Hello World!");
-});
+};
 
-server.listen(3000);
+app.createServer();
